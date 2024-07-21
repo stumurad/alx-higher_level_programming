4e0e3e7b4631c74893bcc3a68ea7f0b86d85c51b
truemorad <truemorad@gmail.com>
2024-07-21 15:18:08 +0400

    m

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,1 +1,5 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+word = str[39:66]
+print (f"{word} with Python")
