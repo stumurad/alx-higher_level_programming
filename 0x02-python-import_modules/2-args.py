e458dfb8615674c17173a67bbcfbc62fb8b1e8ef
truemorad <truemorad@gmail.com>
2024-07-28 15:35:07 +0400

    This

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+if len(argv) == 1:
+    print("{} argument:".format(len(argv)))
+elif len(argv) >= 2:
+    print("{} arguments:".format(len(argv)))
+else:
+    print("{} argument.".format(len(argv)))
+
+for i, arg in enumerate(argv, start=1):
+    print("{}: {}".format(i, arg))
