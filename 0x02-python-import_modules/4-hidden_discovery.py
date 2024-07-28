e27000e5785fc6d649881a9646bed8f72e9b3564
truemorad <truemorad@gmail.com>
2024-07-28 23:52:53 +0400

    This

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import sys
 import hidden_4
 if __name__ == "__main__":
 
-    name = dir(hidden_4.pyc)
+    name = dir(hidden_4)
 filtered_names = [name for name in names if not name.startswith("__")]
 
 for name in filtered_names:
