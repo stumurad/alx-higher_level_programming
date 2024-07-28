89dbc32dbb519437df88eb56c61cc3145207ac05
truemorad <truemorad@gmail.com>
2024-07-28 23:54:50 +0400

    This

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import sys
 import hidden_4
 if __name__ == "__main__":
-
     for name in dir(hidden_4):
         if name[0] != '_' and name[1] != '_':
-    print(name)
+            print(name)
