3d733fbed693dab8cc7b2d27f4876e6ff6a0cd68
truemorad <truemorad@gmail.com>
2024-08-06 20:05:23 +0400

    autocommit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > (len(my_list) - 1):
+        return my_list
+    else:
+        mod_list = my_list
+        mod_list[idx] = element
+        return my_list
