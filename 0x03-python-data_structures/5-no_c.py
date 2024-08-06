3c42d80defbf97ea26c29a9f214366e5a4c8c24b
truemorad <truemorad@gmail.com>
2024-08-06 20:47:58 +0400

    autocommit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = []
     for i in my_string:
-        if i != c and i != C:
+        if i != 'c' and i != 'C':
             result.append(i)
     return ''.join(result)
