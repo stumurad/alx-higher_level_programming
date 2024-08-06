6227617c5970dfe137a96dd9e4de82d2731668f7
truemorad <truemorad@gmail.com>
2024-08-06 19:39:02 +0400

    autocommit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (0)
-    elif idx > len(my_list):
+    elif idx > (len(my_list) -1):
         return (0)
     else:
-        idx = idx + 1
-        idx in my_list
-        return (idx)
+        return (my_list[idx])
