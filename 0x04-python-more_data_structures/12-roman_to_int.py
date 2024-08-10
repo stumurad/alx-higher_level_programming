53413bee528a64d9b1a703f988cf58dad564792f
truemorad <truemorad@gmail.com>
2024-08-11 00:58:19 +0400

    autocommit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,13 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    translate = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
+    trans = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        if i > 0:
-            if translate[roman_string[i]] > translate[roman_string[i - 1]]:
-                num += translate[roman_string[i]]
-                - 2 * translate[roman_string[i - 1]]
+        if i > 0 and trans[roman_string[i]] > translate[roman_string[i - 1]]:
+            num += trans[roman_string[i]]
+            - 2 * trans[roman_string[i - 1]]
         else:
-        num += translate[roman_string[i]]
+            num += trans[roman_string[i]]
     return num
