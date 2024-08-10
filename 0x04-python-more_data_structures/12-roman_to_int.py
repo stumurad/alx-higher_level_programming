596607cef5da18f5344ab5c0b4e35c740286747f
truemorad <truemorad@gmail.com>
2024-08-11 00:53:54 +0400

    autocommit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,10 +6,10 @@ def roman_to_int(roman_string):
                  'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        if i > 0
-        and translate[roman_string[i]] > translate[roman_string[i - 1]]:
-            num += translate[roman_string[i]]
-            - 2 * translate[roman_string[i - 1]]
+        if i > 0:
+            if translate[roman_string[i]] > translate[roman_string[i - 1]]:
+                num += translate[roman_string[i]]
+                - 2 * translate[roman_string[i - 1]]
         else:
-            num += translate[roman_string[i]]
+        num += translate[roman_string[i]]
     return num
