1d7c7b0d64882520a703d5ce6ca433c3e70f1e4b
truemorad <truemorad@gmail.com>
2024-08-11 01:07:31 +0400

    autocommit

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
-    return a_dictionary
+    dictionary = a_dictionary.copy()
+    for key in dictionary:
+        dictionary[key] *= 2
+    return dictionary
